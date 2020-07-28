7fa27cb42dabfb8c632671b5bf7ad38fe0d1bb1c
Jakub Klímek <jakub@jakubklimek.com>
2020-07-28 10:36:32 +0200

    Create profile for MFF UK based on MV CR

---
+++ src/mffuk/defaults.js
@@ -0,0 +1,71 @@
+/**
+ * Sets the defaults for W3C specs
+ */
+export const name = "mffuk/defaults";
+import { coreDefaults } from "../core/defaults.js";
+import { definitionMap } from "../core/dfn-map.js";
+import linter from "../core/linter.js";
+import { rule as privsecSectionRule } from "../core/linter-rules/privsec-section.js";
+import { rule as wptTestsExist } from "../core/linter-rules/wpt-tests-exist.js";
+
+linter.register(privsecSectionRule, wptTestsExist);
+
+const licenses = new Map([
+  [
+    "cc0",
+    {
+      name: "Creative Commons 0 Public Domain Dedication",
+      short: "CC0",
+      url: "https://creativecommons.org/publicdomain/zero/1.0/",
+    },
+  ],
+  [
+    "cc-by",
+    {
+      name: "Creative Commons Attribution 4.0 International Public License",
+      short: "CC-BY",
+      url: "https://creativecommons.org/licenses/by/4.0/legalcode",
+    },
+  ],
+]);
+
+const mffukDefaults = {
+  lint: {
+    "no-headingless-sections": true,
+    "no-http-props": true,
+    "check-punctuation": false,
+    "local-refs-exist": true,
+  },
+  pluralize: false,
+  highlightVars: true,
+  doJsonLd: false,
+  license: "cc-by",
+  specStatus: "base",
+  logos: [  ],
+  addSectionLinks: true,
+  xref: true,
+};
+
+export function run(conf) {
+  // assign the defaults
+  const lint =
+    conf.lint === false
+      ? false
+      : {
+          ...coreDefaults.lint,
+          ...mffukDefaults.lint,
+          ...conf.lint,
+        };
+  Object.assign(conf, {
+    ...coreDefaults,
+    ...mffukDefaults,
+    ...conf,
+    lint,
+  });
+  
+  
+  // TODO: eventually, we want to remove this.
+  // It's here for legacy support of json-ld specs
+  // see https://github.com/w3c/respec/issues/2019
+  Object.assign(conf, { definitionMap });
+}
---
+++ src/mffuk/style.js
@@ -0,0 +1,134 @@
+/* jshint strict: true, browser:true, jquery: true */
+// Module w3c/style
+// Inserts a link to the appropriate W3C style for the specification's maturity level.
+// CONFIGURATION
+//  - specStatus: the short code for the specification's maturity level or type (required)
+
+import { createResourceHint, linkCSS, toKeyValuePairs } from "../core/utils";
+import { pub, sub } from "../core/pubsubhub.js";
+export const name = "mffuk/style.js";
+function attachFixupScript(doc, version) {
+  const script = doc.createElement("script");
+  if (location.hash) {
+    script.addEventListener(
+      "load",
+      () => {
+        window.location = location.hash;
+      },
+      { once: true }
+    );
+  }
+  script.src = `https://ofn.gov.cz/static/js/fixup.js`;
+  doc.body.appendChild(script);
+}
+
+// Make a best effort to attach meta viewport at the top of the head.
+// Other plugins might subsequently push it down, but at least we start
+// at the right place. When ReSpec exports the HTML, it again moves the
+// meta viewport to the top of the head - so to make sure it's the first
+// thing the browser sees. See js/ui/save-html.js.
+function createMetaViewport() {
+  const meta = document.createElement("meta");
+  meta.name = "viewport";
+  const contentProps = {
+    width: "device-width",
+    "initial-scale": "1",
+    "shrink-to-fit": "no",
+  };
+  meta.content = toKeyValuePairs(contentProps).replace(/\"/g, "");
+  return meta;
+}
+
+function createBaseStyle() {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "https://ofn.gov.cz/static/css/base.css";
+  link.classList.add("removeOnSave");
+  return link;
+}
+
+function selectStyleVersion(styleVersion) {
+  let version = "";
+  switch (styleVersion) {
+    case null:
+    case true:
+      version = "2016";
+      break;
+    default:
+      if (styleVersion && !isNaN(styleVersion)) {
+        version = styleVersion.toString().trim();
+      }
+  }
+  return version;
+}
+
+function createResourceHints() {
+  const resourceHints = [
+    {
+      hint: "preconnect", // for styles and scripts.
+      href: "https://data.gov.cz",
+    },
+    {
+      hint: "preload", // all specs need it, and we attach it on end-all.
+      href: "https://ofn.gov.cz/static/js/fixup.js",
+      as: "script",
+    },
+    {
+      hint: "preload", // all specs include on base.css.
+      href: "https://ofn.gov.cz/static/css/base.css",
+      as: "style",
+    }
+  ]
+    .map(createResourceHint)
+    .reduce(function(frag, link) {
+      frag.appendChild(link);
+      return frag;
+    }, document.createDocumentFragment());
+  return resourceHints;
+}
+// Collect elements for insertion (document fragment)
+const elements = createResourceHints();
+
+// Opportunistically apply base style
+elements.appendChild(createBaseStyle());
+if (!document.head.querySelector("meta[name=viewport]")) {
+  // Make meta viewport the first element in the head.
+  elements.prepend(createMetaViewport());
+}
+
+document.head.prepend(elements);
+
+function styleMover(linkURL) {
+  return exportDoc => {
+    const odczStyle = exportDoc.querySelector(`head link[href="${linkURL}"]`);
+    exportDoc.querySelector("head").append(odczStyle);
+  };
+}
+
+export function run(conf) {
+  if (!conf.specStatus) {
+    const warn = "`respecConfig.specStatus` missing. Defaulting to 'base'.";
+    conf.specStatus = "base";
+    pub("warn", warn);
+  }
+
+  // Select between released styles and experimental style.
+  const version = selectStyleVersion(conf.useExperimentalStyles || "2016");
+  // Attach W3C fixup script after we are done.
+  if (version && !conf.noToc) {
+    sub(
+      "end-all",
+      function() {
+        attachFixupScript(document, version);
+      },
+      { once: true }
+    );
+  }
+  const finalVersionPath = version ? `${version}/` : "";
+  const finalStyleURL = `https://ofn.gov.cz/static/css/${conf.specStatus}.css`;
+
+  linkCSS(document, finalStyleURL);
+  // Make sure the ODCZ stylesheet is the last stylesheet, as required by W3C Pub Rules.
+  const moveStyle = styleMover(finalStyleURL);
+  sub("beforesave", moveStyle);
+}
